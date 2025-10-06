6054bd2dd6f34af4777c9505bc639301e631e242
Borhan <Golkarborhan@gmail.com>
2025-10-06 15:54:05 +0330

    Part #10

--- Assets/Js/Main.js
+++ Assets/Js/Main.js
@@ -2,3 +2,53 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, {});
   });
+
+
+
+// fetch('https://jsonplaceholder.typicode.com/users')
+//   .then((response) => response.json())
+//   .then((json) => console.log(json));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // console.log(1);
+ 
+  // new Promise(
+  //   function(resolve, reject){
+  //        setTimeout(function(){
+  //       console.log(2);
+  //       resolve("Succses");
+  //       //reject("Error");
+  //     },1000)
+  //   }
+  // ).then(
+  //   function(response){
+  //     console.log(3);
+  //   }
+  // ).catch(
+  //   function(error){
+  //     console.log("Error 1");
+      
+  //   }
+  // )
+
+  // console.log(4);
+  // console.log(5);
+  
+  
+
+  
+  
--- Assets/Js/User.js
+++ Assets/Js/User.js
@@ -1,16 +1,48 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.collapsible');
-    var instances = M.Collapsible.init(elems, {});
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll(".collapsible");
+  var instances = M.Collapsible.init(elems, {});
 
-    const addUserButton=document.getElementById("add_user_button");
-    addUserButton.addEventListener("click", ()=>{
-      document.querySelector(".add_user_modal").classList.add("show")
-      document.querySelector(".add_user_modal_back").classList.remove("dis-none")
-    })
-
-    document.querySelector(".add_user_modal_back").addEventListener("click", ()=>{
-      document.querySelector(".add_user_modal").classList.remove("show")
-      document.querySelector(".add_user_modal_back").classList.add("dis-none")
-    })
-
+  const addUserButton = document.getElementById("add_user_button");
+  addUserButton.addEventListener("click", () => {
+    document.querySelector(".add_user_modal").classList.add("show");
+    document.querySelector(".add_user_modal_back").classList.remove("dis-none");
   });
+
+  document
+    .querySelector(".add_user_modal_back")
+    .addEventListener("click", () => {
+      document.querySelector(".add_user_modal").classList.remove("show");
+      document.querySelector(".add_user_modal_back").classList.add("dis-none");
+    });
+
+  setTimeout(function () {
+    console.log("Start...");
+
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((json) => {
+        console.log("End...");
+
+        console.log(json)
+
+        let html="";
+
+        for(const user of json){
+          html=html+`<tr>
+            <td>${user.id}</td>
+            <td>${user.name}</td>
+            <td>${user.username}</td>
+            <td>${user.email}</td>
+            <td>${user.address.city}</td>
+            <td>
+              <i class="material-icons orange-text darken-3 m-l-5 m-r-5">edit</i>
+              <i class="material-icons red-text m-l-5 m-r-5">delete_forever</i>
+            </td>
+          </tr>`
+        }
+
+        document.querySelector(".desktop_table_body").innerHTML=html;
+
+      });
+  }, 5000);
+});
